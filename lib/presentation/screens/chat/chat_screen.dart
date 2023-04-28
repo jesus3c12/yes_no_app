74d388696a4988ba66933b4a30d6905004f2ec74
jesus3c12 <jesus3c12@gmail.com>
2023-04-28 15:33:20 -0700

    Se agrega el proyecto

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_messages_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(3.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://preview.redd.it/b9e2mjvbe7h91.jpg?width=640&crop=smart&auto=webp&v=enabled&s=bae1b718973901cc8ab6f09ecc36bbca22fc52eb'),
+          ),
+        ),
+        title: const Text('Leonardo Dicaprio 💙'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                  controller: chatProvider.chatScrollController,
+                    itemCount: chatProvider.messageList.length,
+                    itemBuilder: (context, index) {
+                      final message = chatProvider.messageList[index];
+
+                      return (message.fromWho == FromWho.hers)
+                          ? HerMessageBubble( message : message )
+                          : MyMessageBubble(  message: message );
+                    })),
+            // Caja de texto de mensajes
+            MessageFieldBox(
+              onValue: (value) => chatProvider.sendMessage(value),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
